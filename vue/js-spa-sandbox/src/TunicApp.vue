b2d67696c0ed9205a014a87b5a55fde7dddc353a
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-05-05 18:41:53 -0400

    feat: playing with vue

---
+++ vue/js-spa-sandbox/src/TunicApp.vue
@@ -0,0 +1,24 @@
+<script setup>
+import HorizontalPanels from '@/components/HorizontalPanels.vue'
+import VerticalPanels from '@/components/VerticalPanels.vue'
+</script>
+
+<template>
+  <HorizontalPanels left-initial-width="40%">
+    <template #left>
+      <VerticalPanels topInitialHeight="60%">
+        <template #top>
+          Rune Keyboard
+        </template>
+        <template #bottom>
+          Rune Editor
+        </template>
+      </VerticalPanels>
+    </template>
+    <template #right>
+      Rune Document
+    </template>
+  </HorizontalPanels>
+</template>
+
+<style scoped></style>
