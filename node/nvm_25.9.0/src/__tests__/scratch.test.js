c618e946858789e9db3df67d0b9f879fa6fa033e
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-05-16 22:47:20 -0400

    feat: added jest tests for scratch

---
+++ node/nvm_25.9.0/src/__tests__/scratch.test.js
@@ -0,0 +1,5 @@
+describe('scratch tests', () => {
+    it('doesn\'t really test anything', () => {
+        console.log("This is a test")
+    })
+})
--- node/nvm_25.9.0/src/scratch.js
+++ node/nvm_25.9.0/src/scratch.js
@@ -1,1 +1,9 @@
-console.log("hi there, world");
+
+
+function foo() {
+    console.log("hi there, world");
+}
+
+function foo2() {
+    console.log("hi there, world 2");
+}
